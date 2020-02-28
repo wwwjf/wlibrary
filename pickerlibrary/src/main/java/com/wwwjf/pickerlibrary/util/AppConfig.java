4b29bc60501dbbb3d3e6e4f5e7a29d19a7036450
wwwjf <wwwjf2015@outlook.com>
2020-02-28 16:44:39 +0800

    update attribute

---
+++ pickerlibrary/src/main/java/com/wwwjf/pickerlibrary/util/AppConfig.java
@@ -0,0 +1,8 @@
+package com.wwwjf.pickerlibrary.util;
+
+public class AppConfig {
+
+    public static final String DEBUG_TAG = "liyujiang";// LogCat的标记
+    public static final boolean DEBUG_ENABLE = false;// 是否调试模式
+
+}
